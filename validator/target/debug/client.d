5c9584a4ea6e6f3f420be1cf90227817a195716c
chmthd <chmth@protonmail.com>
2024-05-28 20:57:09 +1000

    Finalise block proposal/voting.

--- validator/target/debug/client.d
+++ validator/target/debug/client.d
@@ -1,1 +1,1 @@
-/home/x/Documents/crypto/PoH/client/target/debug/client: /home/x/Documents/crypto/PoH/client/src/main.rs
+/home/x/Documents/crypto/PoH/validator/target/debug/client: /home/x/Documents/crypto/PoH/validator/src/main.rs
